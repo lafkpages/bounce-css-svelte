8295aceb9a5fe04c3168474a612faf0c5a4955f1
lafkpages <soy.lafk+gh@gmail.com>
2023-09-09 11:18:26 +0200

    List examples

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,26 @@
 <hr />
 
 <h3>Text</h3>
+
 {#each { length: 6 } as _, i}
 	{@const n = i + 1}
 	{@const tag = `h${n}`}
 	<svelte:element this={tag}>Heading {n}</svelte:element>
 {/each}
 <p>Paragraph</p>
+
+<hr />
+
+<h3>Lists</h3>
+
+<ul>
+	{#each { length: 5 } as _, i}
+		<li>List item</li>
+	{/each}
+</ul>
+
+<ol>
+	{#each { length: 5 } as _, i}
+		<li>List item</li>
+	{/each}
+</ol>
