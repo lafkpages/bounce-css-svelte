43fce03c38aaf96ed3b2144aa231a075f9dbdc34
lafkpages <soy.lafk+gh@gmail.com>
2023-09-09 11:36:05 +0200

    Style buttons

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -4,6 +4,25 @@
 	export let color: OptionalColor = null;
 </script>
 
-<button class={color}>
+<button style:--color="var(--bounce-{color}-normal)">
 	<slot />
 </button>
+
+<style>
+	button {
+		font-size: 16px;
+		line-height: 24px;
+		font-family: Clash Display Semibold;
+		color: var(--bounce-black);
+		padding: 8px 32px;
+		border-radius: 2px;
+		outline: 2px solid var(--bounce-black);
+		cursor: pointer;
+		transition: transform 0.3s cubic-bezier(0.18, 0.89, 0.32, 1.5),
+			box-shadow 0.3s cubic-bezier(0.18, 0.89, 0.32, 1.5);
+		width: fit-content;
+		background: var(--color);
+		margin: 8px 0 8px 0;
+		border: none;
+	}
+</style>
