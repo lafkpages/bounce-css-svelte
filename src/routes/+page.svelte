35c540e33e0fa31b03b3d293041b7351bce9b967
lafkpages <soy.lafk+gh@gmail.com>
2023-09-09 11:13:27 +0200

    Show all types of headings

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,10 @@
 <h1>Bounce.css for Svelte</h1>
+
+<hr />
+
+<h3>Text</h3>
+{#each { length: 6 } as _, i}
+	{@const n = i + 1}
+	{@const tag = `h${n}`}
+	<svelte:element this={tag}>Heading {tag}</svelte:element>
+{/each}
